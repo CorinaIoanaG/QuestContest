ab0508377dd05a9a78cab5197100cecf591c8f7f
CorinaIoanaG <corinaghete@gnail.com>
2023-04-05 18:02:41 +0300

    First Commit

---
+++ src/main/java/com/QuestContest/QuestContestApplication.java
@@ -0,0 +1,13 @@
+package com.QuestContest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestContestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuestContest/QuestContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuestContest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
